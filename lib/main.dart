7cfc46c43e37a1cb3bc235de6bee97986d3b08df
DQuintero15 <quinterod164@gmail.com>
2024-05-12 17:48:40 -0500

    feat: Initialize Supabase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:smartaccess_app/src/screens/splash_screen.dart';
+import 'package:smartaccess_app/src/utils/app_constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: AppConstants.supabaseUrl,
+    anonKey: AppConstants.supabaseKey
+  );
+
   runApp(const MainApp());
 }
 
