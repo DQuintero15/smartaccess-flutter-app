8519282207bceab6d6444086fcd513b22456174e
DQuintero15 <quinterod164@gmail.com>
2024-05-12 17:48:01 -0500

    feat: Update API base URL and Supabase configuration

--- lib/src/utils/app_constants.dart
+++ lib/src/utils/app_constants.dart
@@ -4,5 +4,7 @@ class AppConstants {
   static const fontFamily = 'Satoshi';
   static const String smallWhiteLogo = 'assets/images/smartaccess_white_small_logo.png';
   static const String splashAnimation = 'assets/lottie/animation.json';
-  static const String apiBaseUrl = 'https://smartaccessbackend-production.up.railway.app/';
+  static const String apiBaseUrl =  String.fromEnvironment('API_BASE_URL');
+  static const  supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const  supabaseKey = String.fromEnvironment('SUPABASE_KEY');
 }
