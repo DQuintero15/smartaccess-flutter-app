9c51298d14f15ec32389cb1b96ed3ae795201a58
DQuintero15 <quinterod164@gmail.com>
2024-05-12 17:44:21 -0500

    chore: Update login screen UI and integrate Supabase authentication

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,36 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:smartaccess_app/src/services/supabase.service.dart';
 import 'package:smartaccess_app/src/utils/app_color.dart';
 import 'package:smartaccess_app/src/utils/app_constants.dart';
+import 'package:smartaccess_app/src/widgets/button_widget.dart';
+import 'package:smartaccess_app/src/widgets/password_widget.dart';
+import 'package:smartaccess_app/src/widgets/text_field_widget.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
-class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.caribbeanCurrent,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
-              Image.asset(AppConstants.smallWhiteLogo, width: 35, height: 35),
-              const SizedBox(height: 100),
-              const Text(
-                AppConstants.appName,
-                style: TextStyle(
-                  color: AppColor.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: AppConstants.fontFamily,
+        child: SingleChildScrollView(
+          // Wrap with SingleChildScrollView
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60),
+                Image.asset(AppConstants.smallWhiteLogo, width: 40, height: 40),
+                const SizedBox(height: 100),
+                const Text(
+                  AppConstants.appName,
+                  style: TextStyle(
+                    color: AppColor.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: AppConstants.fontFamily,
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 50),
+                TextFieldWidget(
+                    controller: emailController,
+                    hintText: 'Ingresa tu correo electrónico',
+                    type: TextInputType.emailAddress),
+                const SizedBox(height: 20),
+                PasswordWidget(controller: passwordController, hintText: '**********'),
+                const SizedBox(height: 10),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        '¿Olvidaste tu contraseña?',
+                        style: TextStyle(
+                          color: AppColor.white,
+                          fontFamily: AppConstants.fontFamily,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ButtonWidget(
+                  onPressed: () async {
+                    final email = emailController.text;
+                    final password = passwordController.text;
+
+                    final authResponse = await SupabaseService().signInWithEmailAndPassword(email, password);
+
+                    if (authResponse != null) {
+                      print(authResponse);
+                    } else {
+                      print(authResponse);
+                    }
+                    
+
+                  },
+                  text: 'Iniciar sesión',
+                ),
+                const SizedBox(height: 30),
+                ButtonWidget(
+                  onPressed: () {},
+                  text: 'Registrarse',
+                ),
+              ],
+            ),
           ),
         ),
       ),
