9a8d93d94e41c0fe41c1e719bc1e44cd5b1b1113
DQuintero15 <quinterod164@gmail.com>
2024-05-14 05:55:04 -0500

    feat: remove supba to implement provider for state management

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:smartaccess_app/src/services/business_service.dart';
-import 'package:smartaccess_app/src/services/firebase_auth_service.dart';
+import 'package:provider/provider.dart';
+import 'package:smartaccess_app/src/providers/business_provider.dart';
 import 'package:smartaccess_app/src/utils/app_color.dart';
 import 'package:smartaccess_app/src/utils/app_constants.dart';
 
@@ -12,98 +12,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final _businessService = BusinessService();
-  final _firebaseAuthService = FirebaseAuthService();
-
+  @override
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<BusinessProvider>().getBusinessData();
+    });
+
     return Scaffold(
       backgroundColor: AppColor.night,
-      body: FutureBuilder(
-        future: _firebaseAuthService.currentUser?.getIdToken(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
+      body: Consumer<BusinessProvider>(
+        builder: (context, value, child) {
+          if (value.businessData == null) {
             return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
           } else {
-            final token = snapshot.data;
-            if (token != null) {
-              return FutureBuilder(
-                future: _businessService.getBusiness(token),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text('Error: ${snapshot.error}'));
-                  } else {
-                    final businessData = snapshot.data;
-                    return Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: SafeArea(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+            final businessData = value.businessData;
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
                           children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: const Image(
+                                image: AssetImage(
+                                    "assets/images/parking_icon.jpg"),
+                                width: 35,
+                                height: 35,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Row(
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(20),
-                                      child: const Image(
-                                        image: AssetImage(
-                                            "assets/images/parking_icon.jpg"),
-                                        width: 35,
-                                        height: 35,
-                                      ),
-                                    ),
-                                    const SizedBox(width: 10),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          '${businessData?.name}',
-                                          style: const TextStyle(
-                                            color: AppColor.white,
-                                            fontFamily: AppConstants.fontFamily,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 1),
-                                        Text(
-                                          '${businessData?.owner}',
-                                          style: const TextStyle(
-                                            color: AppColor.gray,
-                                            fontFamily: AppConstants.fontFamily,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 1),
-                                        Text(
-                                          'Tarifa: \$${businessData?.coastPerMinute} COP/min',
-                                          style: const TextStyle(
-                                            color: AppColor.gray,
-                                            fontFamily: AppConstants.fontFamily,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
+                                Text(
+                                  '${businessData?.name}',
+                                  style: const TextStyle(
+                                    color: AppColor.white,
+                                    fontFamily: AppConstants.fontFamily,
+                                  ),
                                 ),
-                                const Icon(Icons.notifications,
-                                    color: AppColor.white),
+                                const SizedBox(height: 1),
+                                Text(
+                                  '${businessData?.owner}',
+                                  style: const TextStyle(
+                                    color: AppColor.gray,
+                                    fontFamily: AppConstants.fontFamily,
+                                  ),
+                                ),
+                                const SizedBox(height: 1),
+                                Text(
+                                  'Tarifa: \$${context.read<BusinessProvider>().businessData?.coastPerMinute.toString()} COP/min',
+                                  style: const TextStyle(
+                                    color: AppColor.gray,
+                                    fontFamily: AppConstants.fontFamily,
+                                  ),
+                                ),
                               ],
                             ),
                           ],
                         ),
-                      ),
-                    );
-                  }
-                },
-              );
-            } else {
-              return const Center(child: Text('Error: No se pudo obtener el token'));
-            }
+                        const Icon(Icons.notifications, color: AppColor.white),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
           }
         },
       ),
