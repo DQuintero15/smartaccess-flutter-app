33c86e8ca802fa15bcb12a3af2e535f21fad2741
DQuintero15 <quinterod164@gmail.com>
2024-05-14 07:02:15 -0500

    feat: Add DetectionProvider class for managing detection results

---
+++ lib/src/providers/detection_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:smartaccess_app/src/services/detection_service.dart';
+import 'package:smartaccess_app/src/services/firebase_auth_service.dart';
+
+class DetectionProvider extends ChangeNotifier {
+  final _detectionService = DetectionService();
+  final _firebaseAuthService = FirebaseAuthService();
+
+  String? result;
+
+  String? get detectionResult => result;
+
+  Future<void> getDetection(String secureUrl) async {
+    final token = await _firebaseAuthService.currentUser?.getIdToken();
+
+    if(token == null) {
+      return;
+    }
+
+    final detection = await _detectionService.getDetection(token, secureUrl);
+    if (detection != null) {
+      result = detection;
+      notifyListeners();
+    }
+  }
+
+  void clearDetection() {
+    result = null;
+    notifyListeners();
+  }
+}
