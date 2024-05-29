4a5020edb6d5b76cd7199c0de4bab8435851ae13
DQuintero15 <quinterod164@gmail.com>
2024-05-29 15:16:46 -0500

    feat: Add verification functionality to DetectionProvider

--- lib/src/providers/detection_provider.dart
+++ lib/src/providers/detection_provider.dart
@@ -7,14 +7,33 @@ class DetectionProvider extends ChangeNotifier {
   final _detectionService = DetectionService();
   final _firebaseAuthService = FirebaseAuthService();
 
+  AppDetection? appDetection;
+
+  AppDetection? get appDetectionResult => appDetection;
+
   Detection? result;
 
   Detection? get detectionResult => result;
 
+  Future<void> verifyDetection(String plate) async {
+    final token = await _firebaseAuthService.currentUser?.getIdToken();
+
+    if (token == null) {
+      return;
+    }
+
+    final detection = await _detectionService.verifyDetection(token, plate);
+
+    if (detection != null) {
+      appDetection = detection;
+      notifyListeners();
+    }
+  }
+
   Future<void> getDetection(String secureUrl) async {
     final token = await _firebaseAuthService.currentUser?.getIdToken();
 
-    if(token == null) {
+    if (token == null) {
       return;
     }
 
